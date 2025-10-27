f4b7588b884c507e33c46ec058229ab467852b46
dongdong-byte <kimeee34@gmail.com>
2025-10-27 15:30:16 +0900

    수정

--- src/main/resources/studentCRUD/mapper/StudentMapper.java
+++ src/main/resources/studentCRUD/mapper/StudentMapper.java
@@ -6,15 +6,11 @@ import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 
 import com.kim.ex25_branch.domain.*;
-import org.apache.ibatis.annotations.Select;
-
 @Mapper
 public interface StudentMapper {
 //    Create Read(findAll,findById) Update Delete
     List<Student> findAll();
-   
     Student findById(Long id);
-
     void create(Student student);
     void update(Student student);
     void delete(Long id);
