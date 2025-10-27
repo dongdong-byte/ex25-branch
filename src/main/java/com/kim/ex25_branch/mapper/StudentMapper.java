5adfddf8ce4c3a1fd881f94c89f9aee4d6092238
dongdong-byte <kimeee34@gmail.com>
2025-10-27 15:44:24 +0900

    수정본

--- src/main/java/com/kim/ex25_branch/mapper/StudentMapper.java
+++ src/main/java/com/kim/ex25_branch/mapper/StudentMapper.java
@@ -3,19 +3,48 @@ package com.kim.ex25_branch.mapper;
 
 import java.util.List;
 
-import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.*;
 
 import com.kim.ex25_branch.domain.*;
-import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface StudentMapper {
 //    Create Read(findAll,findById) Update Delete
     List<Student> findAll();
-   
+@Select("SELECT\n" +
+        "        id,\n" +
+        "        name,\n" +
+        "        email,\n" +
+        "        age,\n" +
+        "        created_at , <!-- DB 컬럼명 -->\n" +
+        "        updated_at\n" +
+        "        FROM student\n" +
+        "        WHERE id = #{id}")
     Student findById(Long id);
-
+@Insert("INSERT INTO student (\n" +
+        "        name,\n" +
+        "        email,\n" +
+        "        age,\n" +
+        "        created_at,\n" +
+        "        updated_at\n" +
+        "        )\n" +
+        "        VALUES (\n" +
+        "        #{name},\n" +
+        "        #{email},\n" +
+        "        #{age},\n" +
+        "        NOW(),\n" +
+        "        NOW()\n" +
+        "        )")
     void create(Student student);
+@Update("UPDATE student\n" +
+        "        SET\n" +
+        "        name = #{name},\n" +
+        "        email = #{email},\n" +
+        "        age = #{age},\n" +
+        "        updated_at = NOW()\n" +
+        "        WHERE id = #{id}")
     void update(Student student);
+@Delete(" DELETE FROM student\n" +
+        "        WHERE id = #{id}")
     void delete(Long id);
 }
